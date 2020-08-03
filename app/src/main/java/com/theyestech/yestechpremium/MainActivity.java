3a08a656d177b0258026272c1a44a3960baea7f7
jazper298 <minatosjutsu@gmail.com>
2020-08-03 11:01:51 +0800

    initialize admin navigation

--- app/src/main/java/com/theyestech/yestechpremium/MainActivity.java
+++ app/src/main/java/com/theyestech/yestechpremium/MainActivity.java
@@ -1,14 +1,35 @@
 package com.theyestech.yestechpremium;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+    private Context context;
+
+    private String role;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_announcement, R.id.navigation_video_lib, R.id.navigation_educator_list, R.id.navigation_student_list)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        context = this;
     }
+
 }
---
+++ app/src/main/java/com/theyestech/yestechpremium/fragments/VideoLibFragment.java
@@ -0,0 +1,29 @@
+package com.theyestech.yestechpremium.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.theyestech.yestechpremium.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VideoLibFragment extends Fragment {
+
+    public VideoLibFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video_lib, container, false);
+    }
+}
