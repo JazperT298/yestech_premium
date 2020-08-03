f0a924f2011ef6fe014a0b41c3a29a770969ce27
jazper298 <minatosjutsu@gmail.com>
2020-08-03 16:42:59 +0800

    new announcement dialog and file permissions

--- app/src/main/java/com/theyestech/yestechpremium/fragments/AnnouncementFragment.java
+++ app/src/main/java/com/theyestech/yestechpremium/fragments/AnnouncementFragment.java
@@ -1,19 +1,75 @@
 package com.theyestech.yestechpremium.fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.theyestech.yestechpremium.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.Random;
+
+import es.dmoral.toasty.Toasty;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AnnouncementFragment extends Fragment {
+
+    private View view;
+    private Context context;
+    private ImageView iv_AddAnnouncement;
+    private TextView tv_Filename;
+    private String details = "";
+
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
+
+    private static final int VIDEO_PERMISSION_CODE = 2000;
+    private static final int VIDEO_REQUEST_CODE = 3000;
+
+    private static final int CAMERA_PERMISSION_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+    private static final int DOCUMENT_PERMISSION_CODE = 103;
+    private static final int DOCUMENT_REQUEST_CODE = 104;
+
+    private String storagePermission[];
+    private String cameraPermission[];
+    private Uri selectedFile;
+    private String selectedFilePath = "";
+    private String displayName = "";
+    private File myFile;
 
     public AnnouncementFragment() {
         // Required empty public constructor
@@ -24,6 +80,226 @@ public class AnnouncementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_announcement, container, false);
+        view = inflater.inflate(R.layout.fragment_announcement, container, false);
+        context = getContext();
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initializeUI();
+    }
+
+    private void initializeUI(){
+        iv_AddAnnouncement = view.findViewById(R.id.iv_AddAnnouncement);
+        iv_AddAnnouncement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddAnnouncementDialog();
+            }
+        });
+    }
+
+    private void openAddAnnouncementDialog(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.dialog_add_announcement, null);
+        final EditText et_AddEditDetails;
+        final Button btn_ChooseAddNewsFeedFile,btn_AddEditNewsFeedSave;
+        final ImageView imageView33;
+
+        et_AddEditDetails = dialogView.findViewById(R.id.et_AddEditDetails);
+        //tvHeader = dialogView.findViewById(R.id.tvHeader);
+        btn_ChooseAddNewsFeedFile = dialogView.findViewById(R.id.btn_ChooseAddNewsFeedFile);
+        btn_AddEditNewsFeedSave = dialogView.findViewById(R.id.btn_AddEditNewsFeedSave);
+        imageView33 = dialogView.findViewById(R.id.imageView33);
+        tv_Filename = dialogView.findViewById(R.id.tv_Filename);
+
+        dialogBuilder.setView(dialogView);
+        final AlertDialog b = dialogBuilder.create();
+
+        imageView33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b.hide();
+            }
+        });
+
+        btn_ChooseAddNewsFeedFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectActions();
+            }
+        });
+
+        btn_AddEditNewsFeedSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                details = et_AddEditDetails.getText().toString();
+
+                if (details.isEmpty())
+                    Toasty.warning(context, "Please input news feed details.").show();
+                else {
+                    //saveNewNewsfeed();
+                    b.hide();
+                }
+            }
+        });
+
+        b.show();
+        Objects.requireNonNull(b.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+    private void selectActions() {
+        String[] items = {" Camera ", " Gallery ", " Videos "};
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setTitle("Choose File");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    askCameraPermissions();
+                } else if (which == 1) {
+                    if (!checkStoragePermission()) {
+                        requestStoragePermission();
+                    } else {
+                        selectedFilePath = "";
+                        pickImageGallery();
+                    }
+                }else if (which == 2){
+                    if (!checkStoragePermission()) {
+                        requestStoragePermission();
+                    } else {
+                        askVideoPermissions();
+                    }
+                }
+            }
+        });
+        dialog.create().show();
+    }
+    private void askCameraPermissions(){
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }else  {
+            pickCamera();
+        }
+    }
+    private void askVideoPermissions(){
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, VIDEO_PERMISSION_CODE);
+        }else  {
+            pickVideo();
+        }
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(getActivity(), storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    private void pickImageGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void pickCamera(){
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture,  CAMERA_REQUEST_CODE);//
+    }
+
+    private void pickVideo(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("video/*");
+        startActivityForResult(Intent.createChooser(intent,"Select Video"),VIDEO_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case STORAGE_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (writeStorageAccepted) {
+                        pickImageGallery();
+                    } else {
+                        Toasty.error(context, "Permission denied ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+            case CAMERA_PERMISSION_CODE:
+                if (grantResults.length < 0) {
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (writeStorageAccepted) {
+                        pickCamera();
+                    } else {
+                        Toasty.error(context, "Permission denied ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                selectedFile = data.getData();
+
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = context.getContentResolver().query(selectedFile,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                selectedFilePath = cursor.getString(columnIndex);
+                myFile = new File(selectedFilePath);
+                displayName = myFile.getName();
+                tv_Filename.setText(displayName);
+
+            }else if (requestCode == CAMERA_REQUEST_CODE) {
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+
+                Random r = new Random();
+                int randomNumber = r.nextInt(10000);
+                selectedFilePath = String.valueOf(randomNumber);
+                File filesDir = getContext().getFilesDir();
+                myFile = new File(filesDir, selectedFilePath + ".jpg");
+                displayName = myFile.getName();
+                tv_Filename.setText(displayName);
+                OutputStream os;
+                try {
+                    os = new FileOutputStream(myFile);
+                    image.compress(Bitmap.CompressFormat.JPEG, 100, os);
+                    os.flush();
+                    os.close();
+                } catch (Exception e) {
+                    Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+                }
+            } else if (requestCode == VIDEO_REQUEST_CODE) {
+                selectedFile = data.getData();
+
+                String[] filePathColumn = {MediaStore.Video.Media.DATA};
+
+                Cursor cursor = context.getContentResolver().query(selectedFile,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                selectedFilePath = cursor.getString(columnIndex);
+
+                myFile = new File(selectedFilePath);
+                tv_Filename.setText(myFile.getName());
+            }
+        }
     }
 }
