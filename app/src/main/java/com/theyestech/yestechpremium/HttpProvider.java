3c7dd55cbd8aaf433f45dd963c43204060065ab6
jazper298 <minatosjutsu@gmail.com>
2020-08-03 10:17:41 +0800

    add dependencies and manifest permissions

---
+++ app/src/main/java/com/theyestech/yestechpremium/HttpProvider.java
@@ -0,0 +1,70 @@
+package com.theyestech.yestechpremium;
+
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+public class HttpProvider {
+    private static final String BASE_URL = "https://theyestech.com/controllerClass/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void post(Context context, String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(context, getAbsoluteUrl(url), null, params, null, responseHandler);
+    }
+
+    public static void post2(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void postLogin(Context context, String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(context, getAbsoluteUrl(url), null, params, null, responseHandler);
+    }
+
+    public static void get(Context context, String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(context, getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
+    public static String getBaseURL() {
+        return BASE_URL.replace("api/", "");
+    }
+
+    //Directory Files
+    public static String getNewsfeedDir() {
+        return BASE_URL.replace("controllerClass/", "newsfeed-files/");
+    }
+
+    public static String getVideoLabDir() {
+        return BASE_URL.replace("controllerClass/", "");
+    }
+
+    public static String getProfileDir() {
+        return BASE_URL.replace("controllerClass/", "user_images/");
+    }
+
+    public static String getSubjectDir() {
+        return BASE_URL.replace("controllerClass/", "subject-files/");
+    }
+
+    public static String getTopicDir() {
+        return BASE_URL.replace("controllerClass/", "topic-files/");
+    }
+
+    public static String getQuizDir() {
+        return BASE_URL.replace("controllerClass/", "");
+    }
+
+    public static String getStickerDir() {
+        return BASE_URL.replace("controllerClass/", "img/");
+    }
+
+    public static String getNotesDir() {
+        return BASE_URL.replace("controllerClass/", "notes-files/");
+    }
+}
